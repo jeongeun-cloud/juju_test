14549780abd0dd6c9d39f8fe92004cbdf2c57a0e
kyuwonlee <kwnylee@gmail.com>
2020-06-26 11:58:32 +0900

    호호규원호호

--- gitTest/src/main/java/org/codeck/controller/JujuTest.java
+++ gitTest/src/main/java/org/codeck/controller/JujuTest.java
@@ -3,6 +3,14 @@ package org.codeck.controller;
 public class JujuTest {
 	public static void main(String[] args) {
 		
+		//규원규원
+		int x = 10;
+		int y = 20;
+		System.out.println(x+y);
+		System.out.println(x-y);
+		System.out.println(x*y);
+		System.out.println(x/y);
+		//규원규원 
 	}
 
 
