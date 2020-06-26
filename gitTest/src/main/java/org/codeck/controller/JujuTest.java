77afb187ae1ea72014182f6d962f338504a708aa
kyuwonlee <kwnylee@gmail.com>
2020-06-26 11:42:57 +0900

    ㅁㄴㅇㄹ

--- gitTest/src/main/java/org/codeck/controller/JujuTest.java
+++ gitTest/src/main/java/org/codeck/controller/JujuTest.java
@@ -1,7 +1,10 @@
 package org.codeck.controller;
 
 public class JujuTest {
-public static void main(String[] args) {
-	System.out.println("주주마켓 ");
-}
+	public static void main(String[] args) {
+		
+	}
+
+
+
 }
