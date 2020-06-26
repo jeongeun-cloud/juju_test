c6678a689eca8fe091b2d07a12b65bee3812da3b
dyden <dyden2ehd@naver.com>
2020-06-26 11:58:27 +0900

    '수정은'

--- gitTest/src/main/java/org/codeck/controller/JujuTest.java
+++ gitTest/src/main/java/org/codeck/controller/JujuTest.java
@@ -3,8 +3,9 @@ package org.codeck.controller;
 public class JujuTest {
 	public static void main(String[] args) {
 		
+		
+		String name = "주정은";
+		System.out.println(name + "이 수정함");
+		
 	}
-
-
-
 }
